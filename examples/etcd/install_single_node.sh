83b6d9b175bca422e0b86b106e8b049a1961dba0
Xu Wenqing <xuwq@seagroup.com>
2022-02-18 19:19:30 +0800

    update etcd scripts

--- examples/etcd/install_single_node.sh
+++ examples/etcd/install_single_node.sh
@@ -1,4 +1,5 @@
 # install etcd within docker as it's simple 
-# Need env var ETCD_VERSION
+# Need env var ETCD_VERSION ETCD_LISTEN_IP
 
-docker run -d --net host --name etcd quay.io/coreos/etcd:$ETCD_VERSION /usr/local/bin/etcd --name node1 --initial-advertise-peer-urls http://127.0.0.1:2380 --listen-peer-urls http://0.0.0.0:2380 --advertise-client-urls http://127.0.0.1:2379 --listen-client-urls http://0.0.0.0:2379 --initial-cluster node1=http://127.0.0.1:2380
+
+docker run -d --net host --name etcd quay.io/coreos/etcd:$ETCD_VERSION /usr/local/bin/etcd --name node1 --advertise-client-urls http://$ETCD_LISTEN_IP:2379  --listen-peer-urls http://0.0.0.0:2380 --listen-client-urls http://0.0.0.0:2379 --auto-compaction-mode=periodic --auto-compaction-retention=1h 
